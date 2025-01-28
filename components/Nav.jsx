767ed86aca6de5be7c63faa192ff5b108875b2fa
S JEVANANDHAM <231501069@rajalakshmi.edu.in>
2025-01-28 09:03:52 +0530

    added the home route

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,24 +5,18 @@ import { useRouter } from "next/navigation"; // keep using the standard useRoute
 import Image from "next/image";
 
 export default function Nav() {
-    const [mounted, setMounted] = useState(false);
     const router = useRouter();
-
-    useEffect(() => {
-        setMounted(true); // Ensure useRouter is only used after mounting
-    }, []);
 
     const handleLogout = () => {
         router.push('/Router');
     };
 
-    if (!mounted) return null;  // Prevent rendering until the component has mounted
 
     return (
         <div>
             <header className="bg-white shadow-md">
-                <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-                    <div className="flex flex-row items-center gap-3">
+                <div className="container mx-auto px-4 py-4 flex justify-between items-center cursor-pointer">
+                    <div className="flex flex-row items-center gap-3" onClick={()=>{router.push('/')}}>
                         <Image src="/logo.png" width={50} height={10} alt="Wellness 360 logo" />
                         <h1 className="text-2xl font-bold text-gray-800">Wellness 360</h1>
                     </div>
