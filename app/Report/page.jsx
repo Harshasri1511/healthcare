a3f0ebbf2d09667b812d1660a24426990023947c
S JEVANANDHAM <231501069@rajalakshmi.edu.in>
2025-01-29 22:44:03 +0530

    Changed Report page

--- app/Report/page.jsx
+++ app/Report/page.jsx
@@ -11,34 +11,46 @@ export default function Report(){
     }
 
     return(
-        <div className="p-10 min-h-screen bg-gray-100">
-            <form onSubmit={(e)=>{
-            e.preventDefault();
-            console.log(e)
-            }} className="flex flex-col gap-5 bg-white shadow-md p-5 rounded-lg">
-                <h1 className="text-2xl font-semibold text-center">
-                    Add your Medical Reports
-                </h1>
-                <h1 className="text-xl font-semibold text-left text-gray-800">Add Report :</h1>
-                <input
-                type="file"
-                accept="image/*,.pdf,.doc,.docx,.txt"
-                className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 transition-colors duration-150 ease-out" required
-                 />
-                <h1 className="text-xl font-semibold text-left text-gray-800">Choose the type :</h1>
-                <select value = {selectedReport} onChange={handlechange} required className="hover:bg-gray-100 px-1 py-1 rounded-md">
-                    <option value = "" disabled>Select the Report Type</option>
-                    <option value="lood pressure">Blood pressure</option>
-                    <option value="Blood Sugar<">Blood Sugar</option>
-                    <option value="Genetic Report">Genetic Report</option>
-                    <option value="Hormonals Profile">Hormonals Profile</option>
-                </select>
-                <div className="flex justify-center">
-                    <button className="bg-blue-600 p-2 rounded-lg text-white w-20 hover:bg-blue-700">Submit</button>
+            <div className="p-10 min-h-screen bg-gray-100 xs:flex xs:justify-center lg:flex-row lg:justify-between">
+                <div className="inline-block">
+                    <form onSubmit={(e)=>{
+                    e.preventDefault();
+                    console.log(e)
+                    }} className="flex flex-col gap-5 bg-white shadow-md p-8 rounded-lg">
+                    <h1 className="text-2xl font-semibold text-center">
+                        Add your Medical Reports
+                    </h1>
+                    <h1 className="text-xl font-semibold text-left text-gray-800">Add Report :</h1>
+                    <input
+                    type="file"
+                    accept="image/*,.pdf,.doc,.docx,.txt"
+                    className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 transition-colors duration-150 ease-out" required
+                    />
+                    <h1 className="text-xl font-semibold text-left text-gray-800">Choose the type :</h1>
+                    <select value = {selectedReport} onChange={handlechange} required className="hover:bg-gray-100 px-1 py-1 rounded-md">
+                        <option value = "" disabled>Select the Report Type</option>
+                        <option value="lood pressure">Blood pressure</option>
+                        <option value="Blood Sugar<">Blood Sugar</option>
+                        <option value="Genetic Report">Genetic Report</option>
+                        <option value="Hormonals Profile">Hormonals Profile</option>
+                    </select>
+                    <div className="flex justify-center">
+                        <button className="bg-blue-600 p-2 rounded-lg text-white w-20 hover:bg-blue-700">Submit</button>
+                    </div>
+                    
+                    </form>
                 </div>
-                
-            </form>
-        </div>
+                <div className="xs:hidden lg:inline-block">
+                   <div className="p-10 bg-white">
+                   <link type="image/png" sizes="32x32" rel="icon" href=".../icons8-user profile-ios7-32.png"></link>
+                   <a target="_blank" href="https://icons8.com/icon/7820/male-user">User Profile</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+                    </div> 
+                </div>
+
+
+            </div>
+        
+        
         
     );
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -15,13 +15,13 @@ export default function Nav() {
     return (
         <div>
             <header className="bg-white shadow-md">
-                <div className="container mx-auto px-4 py-4 flex justify-between items-center cursor-pointer">
+                <div className="px-6 py-4 flex items-center justify-between cursor-pointer">
                     <div className="flex flex-row items-center gap-3" onClick={()=>{router.push('/')}}>
                         <Image src="/logo.png" width={50} height={10} alt="Wellness 360 logo" />
                         <h1 className="text-2xl font-bold text-gray-800">Wellness 360</h1>
                     </div>
 
-                    <div className="flex flex-row gap-4">
+                    <div className="flex flex-row gap-4 justify-end">
                         <button
                             className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
                             onClick={() => { router.push('/Report') }}
